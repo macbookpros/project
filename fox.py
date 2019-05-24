21ce55364ae331c968c563c499d95556782b80a5
KishoreBaktha <kishorebaktha@gmail.com>
2019-05-24 11:57:56 +0200

    solved reading

--- fox.py
+++ fox.py
@@ -2,6 +2,7 @@ from mpi4py import MPI
 from Config import Config
 import math
 import sys
+from array import array
 import struct
 from decimal import Decimal
 import numpy
@@ -25,9 +26,9 @@ config.WORLD_SIZE=nproc
 if config.world_rank == 0:
     ba = bytearray(2)
     config. A_file.Iread(ba)
-    size = struct.unpack('<H',ba)
-    config.A_dims=[size[0],size[0]]
-    config.B_dims = [size[0],size[0]]
+    size = struct.unpack('<H',ba)[0]
+    config.A_dims=[size,size]
+    config.B_dims = [size,size]
 
 config.A_dims=comm.bcast(config.A_dims, root=0)
 config.B_dims=comm.bcast(config.B_dims, root=0)
@@ -35,8 +36,6 @@ config.B_dims=comm.bcast(config.B_dims, root=0)
 config.dim = [math.sqrt(config.WORLD_SIZE),math.sqrt(config.WORLD_SIZE)]
 
 wrap_around=[0,1]
-
-
 
 config.grid_comm=comm.Create_cart(config.dim,periods=wrap_around)
 config.grid_rank=config.grid_comm.Get_rank()
@@ -48,7 +47,7 @@ config.col_comm=config.grid_comm.Sub(free_coords)
 config.col_rank=config.col_comm.Get_rank()
 
 config.matrix_size= config.A_dims[0]*config.A_dims[0]
-config.local_dims=[config.A_dims[0]/config.dim[0],config.A_dims[0]/config.dim[0]]
+config.local_dims=[int(config.A_dims[0]/config.dim[0]),int(config.A_dims[0]/config.dim[0])]
 
 config.local_size=int(config.local_dims[0]*config.local_dims[1])
 
@@ -57,19 +56,21 @@ config.coords=config.grid_comm.Get_coords(config.grid_rank)
 config.my_row=config.coords[0]
 config.my_col=config.coords[1]
 
-starts = [config.my_row*(config.local_dims[0]),config.my_col*(config.local_dims[0])]
+starts = [config.my_row*int(config.local_dims[0]),config.my_col*int(config.local_dims[0])]
 
-config.block=MPI.DOUBLE.Create_subarray(config.A_dims,config.local_dims, starts, order=MPI.ORDER_C)
+if config.world_rank ==2:
+  print("my row and column is %d %d and %d and starts is %d %d " ,(config.my_row,config.my_col,int(config.local_dims[0]),starts[0],starts[1]))
+config.block=MPI.FLOAT.Create_subarray(config.A_dims,config.local_dims, starts, order=MPI.ORDER_C)
 config.block.Commit()
 #config.C=[0]*config.local_size
-
-
-config.A_file.Set_view(disp=4,etype=MPI.DOUBLE,filetype=config.block,datarep="native",info=MPI.INFO_NULL)
-config.B_file.Set_view(disp=4,etype=MPI.DOUBLE,filetype=config.block,datarep="native",info=MPI.INFO_NULL)
-config.A=numpy.ndarray(int(config.local_dims[0] *config.local_dims[0]*8),dtype=float)
-config.B=numpy.ndarray(int(config.local_dims[0] *config.local_dims[0]*4),dtype=float)
-config.C=numpy.ndarray(int(config.local_dims[0] *config.local_dims[0]*4),dtype=float)
-
+config.A_file.Set_view(disp= MPI.INT.Get_size()*2,filetype=config.block)
+config.B_file.Set_view(disp= MPI.INT.Get_size()*2,filetype=config.block)
+#config.A=numpy.ndarray(int(config.local_dims[0] *config.local_dims[0]),dtype=float)
+#config.B=numpy.ndarray(int(config.local_dims[0] *config.local_dims[0]),dtype=float)
+#config.C=numpy.ndarray(int(config.local_dims[0] *config.local_dims[0]),dtype=float)
+config.A=array('f',[0.0]*int(config.local_dims[0] *config.local_dims[0]))
+config.B=array('f',[0.0]*int(config.local_dims[0] *config.local_dims[0]))
+config.C=array('f',[0.0]*int(config.local_dims[0] *config.local_dims[0]))
 config.A_file.Read_all(config.A)
 config.B_file.Read_all(config.B)
 #print(config.A)
@@ -77,8 +78,10 @@ config.B_file.Read_all(config.B)
 
 config.A_file.Close()
 config.B_file.Close()
-for i in range(0, int(config.local_dims[0])):
-    for j in range(0, int(config.local_dims[0])):
-        print(config.A[int(i * config.local_dims[0] + j)])
-config.compute_fox()
+if config.world_rank ==0:
+   for i in range(0, int(config.local_dims[0])):
+       for j in range(0, int(config.local_dims[0])):
+           print(config.A[int(i * config.local_dims[0] + j)])
+
+#config.compute_fox()
 
